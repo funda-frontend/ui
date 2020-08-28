c59a74a787aaecb44a4408c6d0a819eb80338dcc
jpsc <joao@funda.nl>
2020-08-28 17:16:03 +0200

    fix(tailwind): remove example size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ const extendConfig = require('@funda/tailwind-config').extendConfig
 module.exports = extendConfig({
   theme: {
     extend: {
-      padding: {
-        33: '2rem'
-      },
       height: {
         11: '2.75rem'
       }
