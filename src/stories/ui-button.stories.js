f48d4e7846ddd1e46135249faef2f6533ac8c007
jpsc <joao@funda.nl>
2020-09-01 15:51:17 +0200

    fix(button): simplify button styling

--- src/stories/ui-button.stories.js
+++ src/stories/ui-button.stories.js
@@ -1,41 +1,28 @@
-import UiButtonPrimary from '@/components/ui-button-primary.vue';
-import UiButtonPrimaryAlt from '@/components/ui-button-primary-alt.vue';
-import UiButtonSecondary from '@/components/ui-button-secondary.vue';
-import UiButtonSecondaryAlt from '@/components/ui-button-secondary-alt.vue';
-import UiButtonTertiary from '@/components/ui-button-tertiary.vue';
-import UiButtonTransparent from '@/components/ui-button-transparent.vue';
-
+import UiButton from '@/components/ui-button.vue';
 
 export default {
-  title: 'Buttons',
+  title: 'Inputs',
+  component: UiButton,
+  argTypes: {
+    tone: { 
+      control: { 
+        type: 'select',
+        options: [
+          'primary', 'primary-alt', 'secondary', 'secondary-alt', 'tertiary', 'transparent'
+        ] 
+      } 
+    },
+  },
 };
 
-export const Primary = () => ({
-  components: { UiButtonPrimary },
-  template: '<ui-button-primary>Testing button text</ui-button-primary>',
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { UiButton },
+  template: '<ui-button :tone="tone">Example button</ui-button>',
 });
 
-export const PrimaryAlt = () => ({
-  components: { UiButtonPrimaryAlt },
-  template: '<ui-button-primary-alt>Testing button text</ui-button-primary-alt>',
-});
+export const Button = Template.bind({});
+Button.args = {
+  tone: 'primary',
+};
 
-export const Secondary = () => ({
-  components: { UiButtonSecondary },
-  template: '<ui-button-secondary>Testing button text</ui-button-secondary>',
-});
-
-export const SecondaryAlt = () => ({
-  components: { UiButtonSecondaryAlt },
-  template: '<ui-button-secondary-alt>Testing button text</ui-button-secondary-alt>',
-});
-
-export const Tertiary = () => ({
-  components: { UiButtonTertiary },
-  template: '<ui-button-tertiary>Testing button text</ui-button-tertiary>',
-});
-
-export const Transparent = () => ({
-  components: { UiButtonTransparent },
-  template: '<ui-button-transparent>Testing button text</ui-button-transparent>',
-});
