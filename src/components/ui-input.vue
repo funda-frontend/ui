8781f1af3e828c4dac70b9b58755f773f0bb3ce1
Renan Carvalho <renan@fre-14-872.fundango.nl>
2020-11-04 12:37:47 +0100

    Input component wip

---
+++ src/components/ui-input.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="ui-container">
+        <input
+            :type="type"
+            :value="value"
+            :class="isValid !== true ? 'ui-border-red-1' : 'ui-border-light-1'"
+            class="ui-container ui-rounded-sm ui-rounded-sm ui-border ui-bg-white ui-shadow-sm ui-p-2 ui-pl-4 ui-outline-none ui-text-base ui-font-normal"
+            :placeholder="placeHolder"
+            v-bind="$attrs"
+            v-on="$listeners"
+        />
+        <p v-if="!isValid" class="ui-text-red-1 ui-mt-1">
+            {{ validationMessage }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        type: {
+            type: String,
+            default: 'text',
+            validator: (value) => ['text', 'number'].includes(value),
+        },
+        value: {
+            type: String,
+        },
+        placeHolder: {
+            type: String,
+        },
+        isValid: {
+            type: Boolean,
+            default: true,
+        },
+        validationMessage: {
+            type: String,
+        },
+    },
+};
+</script>
---
+++ src/stories/ui-input.stories.js
@@ -0,0 +1,50 @@
+import UiInput from '@/components/ui-input.vue';
+
+export default {
+    title: 'Forms/Input',
+    component: UiInput,
+    argTypes: {
+        type: {
+            control: {
+                type: 'select',
+                options: ['text', 'number'],
+            },
+        },
+        isValid: {
+            control: {
+                options: [true, false],
+            },
+        },
+        validationMessage: {
+            control: {
+                type: 'text',
+            },
+        },
+        value: {
+            control: {
+                type: 'text',
+            },
+        },
+        placeHolder: {
+            control: {
+                type: 'text',
+            },
+        },
+    },
+};
+
+const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { UiInput },
+    template:
+        '<ui-input :type="type" :isValid="isValid" :value="value" :placeHolder="placeHolder" :validationMessage="validationMessage" >lorem ipsum sitamet doloris.</ui-input>',
+});
+
+export const Input = Template.bind({});
+Input.args = {
+    type: 'text',
+    isValid: true,
+    value: 'lorem ipsum sitamet doloris.',
+    placeHolder: 'PlaceHolder',
+    validationMessage: 'Ooops',
+};
