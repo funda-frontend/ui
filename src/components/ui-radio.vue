2b57ae1c16f5320b4e0494ca3c9c3576a6394751
Renan Carvalho <renan@fre-14-872.home>
2020-11-13 09:25:55 +0100

    Refactoring css and html structure

--- src/components/ui-radio.vue
+++ src/components/ui-radio.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         <div v-for="item in items" :key="item.value" class="py-2">
-            <input
-                :id="item.value"
-                name="funda_radio_input"
-                class="hidden"
-                type="radio"
-                v-bind="$attrs"
-            />
             <ui-label
                 class="cursor-pointer flex py-1 hover:text-blue-1"
                 :for="item.value"
             >
-                <img
-                    class="bg-blue-5 mr-2 mt-1 w-4 h-4 border border-blue-2 rounded-3xl"
+                <input
+                    :id="item.value"
+                    name="funda_radio_input"
+                    class="hidden"
+                    type="radio"
+                    v-bind="$attrs"
                 />
+                <div
+                    class="bg-blue-5 mr-2 mt-1 w-4 h-4 border border-blue-2 rounded-3xl"
+                ></div>
                 {{ item.label }}
             </ui-label>
         </div>
@@ -39,23 +39,28 @@ export default {
 };
 </script>
 <style scoped>
-input[type='radio']:checked ~ label > img {
-    background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiMwMDUxN2YiLz4KPC9zdmc+Cg==);
+label {
+    --input_checked: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiMwMDUxN2YiLz4KPC9zdmc+Cg==);
+    --input_hover: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiNjMWRiZTYiLz4KPC9zdmc+Cg==);
+    --input-disabled: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiM5OTkiLz4KPC9zdmc+Cg==);
 }
-input[type='radio']:hover ~ label > img {
-    background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiNjMWRiZTYiLz4KPC9zdmc+Cg==);
+input[type='radio']:checked ~ div {
+    background-image: var(--input_checked);
 }
-input[type='radio']:checked:hover ~ label > img {
-    background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiMwMDUxN2YiLz4KPC9zdmc+Cg==);
+input[type='radio']:hover ~ div {
+    background-image: var(--input_hover);
 }
-input[type='radio']:disabled ~ label > img {
-    background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiM5OTkiLz4KPC9zdmc+Cg==);
+input[type='radio']:checked:hover ~ div {
+    background-image: var(--input_checked);
+}
+input[type='radio']:disabled ~ div {
+    background-image: var(--input-disabled);
     border-color: #ccc;
 }
-input[type='radio']:disabled:hover ~ label > img {
-    background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+CiAgPGNpcmNsZSBjeD0iOCIgY3k9IjgiIHI9IjQiIGZpbGw9IiM5OTkiLz4KPC9zdmc+Cg==);
+input[type='radio']:disabled:hover ~ div {
+    background-image: var(--input-disabled);
 }
-input[type='radio']:disabled ~ label {
+input[type='radio']:disabled ~ div {
     color: #999;
 }
 </style>
