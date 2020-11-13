24d48434632663c597ee311f7c2e8a99f8b324a5
Bart de Rijk <bartderijk@funda.nl>
2020-11-13 07:33:44 +0100

    feat(data-list): wip add basics for data list

---
+++ src/components/ui-data-list.vue
@@ -0,0 +1,32 @@
+<template>
+    <dl 
+        class="fd-data-list"
+        :class="{ 'fd-data-list--card': card }"
+    >
+        <template v-for="(item, i) in data">
+            <div :key="i">
+                <dt class="fd-data-list__dt">
+                    {{ Object.keys(item)[0] }}
+                </dt>
+                <dd class="fd-data-list__dd">
+                    {{ item[Object.keys(item)[0]] }} 
+                </dd>
+            </div>
+        </template>
+    </dl>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            type: Boolean,
+            default: false,
+        },
+        data: {
+            type: Array,
+            required: true,
+        },
+    }
+}
+</script>
---
+++ src/stories/ui-data-list.stories.js
@@ -0,0 +1,28 @@
+import UiDataList from '@/components/ui-data-list.vue';
+
+export default {
+    title: 'Layout/Data list',
+    component: UiDataList,
+    argTypes: {
+        card: {
+            control: {
+                options: [true, false],
+            },
+        },
+    },
+};
+
+const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { UiDataList },
+    template:
+        '<ui-data-list :card="card" :data="data"></ui-data-list>',
+});
+
+export const DataList = Template.bind({});
+DataList.args = {
+    data: [
+        { Name: 'Jane Doe' }, { Phone: '020 561 8200' }
+    ],
+    card: false,
+};
