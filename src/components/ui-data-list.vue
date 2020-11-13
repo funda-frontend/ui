db81495a95c82fce83e3124973355341eb74329f
Bart de Rijk <bartderijk@funda.nl>
2020-11-13 10:43:59 +0100

    feat(data-list): add tailwind styles

--- src/components/ui-data-list.vue
+++ src/components/ui-data-list.vue
@@ -1,18 +1,19 @@
 <template>
-    <dl 
-        class="fd-data-list"
-        :class="{ 'fd-data-list--card': card }"
-    >
-        <template v-for="(item, i) in data">
-            <div :key="i">
-                <dt class="fd-data-list__dt">
-                    {{ Object.keys(item)[0] }}
-                </dt>
-                <dd class="fd-data-list__dd">
-                    {{ item[Object.keys(item)[0]] }} 
-                </dd>
-            </div>
-        </template>
+    <dl class="md:flex-wrap w-full md:flex border-t border-light-2">
+        <div v-for="(item, i) in data" :key="i" class="w-full md:flex">
+            <dt
+                class="fd-data-list__dt w-full block text-dark-3 font-normal md:py-3 pt-3 md:border-b border-light-2 md:max-w-1/3 md:flex-1/3"
+                :class="{ 'md:px-6 px-5': card }"
+            >
+                {{ Object.keys(item)[0] }}
+            </dt>
+            <dd
+                class="fd-data-list__dd w-full block md:py-3 border-b border-light-2 pb-3 md:max-w-2/3 md:flex-2/3"
+                :class="{ 'md:px-6 px-5 md:pl-0': card }"
+            >
+                {{ item[Object.keys(item)[0]] }}
+            </dd>
+        </div>
     </dl>
 </template>
 
@@ -27,6 +28,14 @@ export default {
             type: Array,
             required: true,
         },
-    }
-}
+    },
+};
 </script>
+
+<style scoped>
+.fd-data-list__dt,
+.fd-data-list__dd {
+    -webkit-margin-start: 0;
+    margin-inline-start: 0;
+}
+</style>
--- src/stories/ui-data-list.stories.js
+++ src/stories/ui-data-list.stories.js
@@ -15,14 +15,11 @@ export default {
 const Template = (args, { argTypes }) => ({
     props: Object.keys(argTypes),
     components: { UiDataList },
-    template:
-        '<ui-data-list :card="card" :data="data"></ui-data-list>',
+    template: '<ui-data-list :card="card" :data="data"></ui-data-list>',
 });
 
 export const DataList = Template.bind({});
 DataList.args = {
-    data: [
-        { Name: 'Jane Doe' }, { Phone: '020 561 8200' }
-    ],
+    data: [{ Name: 'Jane Doe' }, { Phone: '020 561 8200' }],
     card: false,
 };
