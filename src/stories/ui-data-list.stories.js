2726ddf453a5a9b0fba91ad786bd506b7247030a
Bart de Rijk <bartderijk@funda.nl>
2020-11-18 10:07:56 +0100

    fix(data-list): improve component naming

--- src/stories/ui-data-list.stories.js
+++ src/stories/ui-data-list.stories.js
@@ -1,6 +1,6 @@
 import UiDataList from '@/components/ui-data-list.vue';
-import UiDt from '@/components/ui-dt.vue';
-import UiDd from '@/components/ui-dd.vue';
+import UiDataTerm from '@/components/ui-data-term.vue';
+import UiDataDescription from '@/components/ui-data-description.vue';
 
 export default {
     title: 'Layout/Data list',
@@ -18,16 +18,20 @@ const Template = (args, { argTypes }) => ({
     props: Object.keys(argTypes),
     components: {
         UiDataList,
-        UiDt,
-        UiDd,
+        UiDataTerm,
+        UiDataDescription,
     },
     data: () => ({
         listData: [{ Name: 'Jane Doe' }, { Phone: '020 561 8200' }],
     }),
     template: `<ui-data-list>
             <template v-for="(item, i) in listData">
-                <ui-dt :card="card">{{ Object.keys(item)[0] }}</ui-dt>
-                <ui-dd :card="card">{{ item[Object.keys(item)[0]] }}</ui-dd>
+                <ui-data-term :card="card">
+                    {{ Object.keys(item)[0] }}
+                </ui-data-term>
+                <ui-data-description :card="card">
+                    {{ item[Object.keys(item)[0]] }}
+                </ui-data-description>
             </template>
         </ui-data-list>`,
 });
