7bb5c23b3a8f11191d8764ee7e3674f9a47bf6ea
Bart de Rijk <bartderijk@funda.nl>
2020-11-19 14:25:44 +0100

    feat(tooltip): add background color options

--- src/components/ui-tooltip.vue
+++ src/components/ui-tooltip.vue
@@ -1,15 +1,40 @@
 <template>
     <div
         role="tooltip"
-        class="absolute w-full p-3 max-w-sm rounded-sm z-50 shadow bg-white"
+        class="absolute w-full p-3 max-w-sm rounded-sm z-50 shadow"
+        :class="backgroundClass"
     >
         <slot />
         <div
-            class="bg-current absolute bg-white w-4 h-4 left-1/2 overflow-hidden transform -translate-x-1/2 translate-y-4 bottom-0"
+            class="bg-current absolute w-4 h-4 left-1/2 overflow-hidden transform -translate-x-1/2 translate-y-4 bottom-0"
         >
             <div
                 class="absolute w-3 h-3 shadow left-1/2 top-0 transform rotate-45 -translate-x-1/2 -translate-y-1/2"
+                :class="backgroundClass"
             ></div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        background: {
+            type: String,
+            default: 'white',
+            validator: (value) => ['white', 'blue', 'light'].includes(value),
+        },
+    },
+    computed: {
+        backgroundClass() {
+            const specificClasses = {
+                white: 'bg-white',
+                blue: 'bg-blue-5',
+                light: 'bg-light-3',
+            }[this.background];
+
+            return specificClasses;
+        },
+    },
+};
+</script>
--- src/stories/ui-tooltip.stories.js
+++ src/stories/ui-tooltip.stories.js
@@ -3,14 +3,26 @@ import UiTooltip from '@/components/ui-tooltip.vue';
 export default {
     title: 'Informational/Tooltip',
     component: UiTooltip,
+    argTypes: {
+        background: {
+            control: {
+                type: 'select',
+                options: ['white', 'blue', 'light'],
+            },
+        },
+    },
 };
 
-const Template = () => ({
+const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
     components: { UiTooltip },
-    template: `<ui-tooltip>
+    template: `<ui-tooltip :background="background">
                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
                    vestibulum turpis enim, eget tempor tellus placerat at.
                </ui-tooltip>`,
 });
 
 export const Tooltip = Template.bind({});
+Tooltip.args = {
+    background: 'light',
+};
