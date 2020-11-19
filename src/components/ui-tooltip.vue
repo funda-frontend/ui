7714670b0d30eb361d72f56250a62098a232329a
Bart de Rijk <bartderijk@funda.nl>
2020-11-19 17:09:32 +0100

    feat(tooltip): added popper WIP

--- src/components/ui-tooltip.vue
+++ src/components/ui-tooltip.vue
@@ -1,28 +1,53 @@
 <template>
-    <div
-        role="tooltip"
-        class="absolute w-full p-3 max-w-sm rounded-sm z-50 shadow"
-        :class="backgroundClass"
+    <popper
+        :trigger="trigger"
+        :options="{
+            placement: 'bottom',
+            modifiers: { offset: { offset: '0,10px' } },
+        }"
     >
-        <slot />
         <div
-            class="bg-current absolute w-4 h-4 left-1/2 overflow-hidden transform -translate-x-1/2 translate-y-4 bottom-0"
+            role="tooltip"
+            class="popper absolute w-full p-3 max-w-sm rounded-sm z-50 shadow"
+            :class="backgroundClass"
         >
+            <slot />
             <div
-                class="absolute w-3 h-3 shadow left-1/2 top-0 transform rotate-45 -translate-x-1/2 -translate-y-1/2"
-                :class="backgroundClass"
-            ></div>
+                class="bg-current absolute w-4 h-4 left-1/2 overflow-hidden transform -translate-x-1/2 translate-y-4 bottom-0"
+            >
+                <div
+                    class="absolute w-3 h-3 shadow left-1/2 top-0 transform rotate-45 -translate-x-1/2 -translate-y-1/2"
+                    :class="backgroundClass"
+                ></div>
+            </div>
         </div>
-    </div>
+
+        <button slot="reference">
+            Reference Element
+        </button>
+    </popper>
 </template>
 
 <script>
+import Popper from 'vue-popperjs';
+
 export default {
+    components: {
+        Popper,
+    },
     props: {
         background: {
             type: String,
             default: 'white',
             validator: (value) => ['white', 'blue', 'light'].includes(value),
+        },
+        trigger: {
+            type: String,
+            default: 'hover',
+            validator: (value) =>
+                ['hover', 'clickToOpen', 'clickToToggle', 'focus'].includes(
+                    value
+                ),
         },
     },
     computed: {
--- src/stories/ui-tooltip.stories.js
+++ src/stories/ui-tooltip.stories.js
@@ -10,19 +10,26 @@ export default {
                 options: ['white', 'blue', 'light'],
             },
         },
+        trigger: {
+            control: {
+                type: 'select',
+                options: ['hover', 'clickToOpen', 'clickToToggle', 'focus'],
+            },
+        },
     },
 };
 
 const Template = (args, { argTypes }) => ({
     props: Object.keys(argTypes),
     components: { UiTooltip },
-    template: `<ui-tooltip :background="background">
+    template: `<ui-tooltip :trigger="trigger" :background="background">
                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
                    vestibulum turpis enim, eget tempor tellus placerat at.
-               </ui-tooltip>`,
+              </ui-tooltip>`,
 });
 
 export const Tooltip = Template.bind({});
 Tooltip.args = {
     background: 'light',
+    trigger: 'hover',
 };
