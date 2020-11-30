db96f0a7f483a0073be455ab0644d9551c4face5
Bart de Rijk <bartderijk@funda.nl>
2020-11-30 12:54:55 +0100

    fix(button-group): improve css

--- src/components/ui-button-group.vue
+++ src/components/ui-button-group.vue
@@ -9,16 +9,21 @@
 </template>
 
 <style scoped>
-.fd-btn-group button:not(:last-child),
-.fd-btn-group a:not(:last-child) {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
+.fd-btn-group button,
+.fd-btn-group a {
+    border-radius: 0;
+    margin-left: -0.0625rem;
 }
 
-.fd-btn-group button:not(:first-child),
-.fd-btn-group a:not(:first-child) {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-    margin-left: -0.0625rem;
+.fd-btn-group button:first-child,
+.fd-btn-group a:first-child {
+    border-top-left-radius: 0.125rem;
+    border-bottom-left-radius: 0.125rem;
+}
+
+.fd-btn-group button:last-child,
+.fd-btn-group a:last-child {
+    border-top-right-radius: 0.125rem;
+    border-bottom-right-radius: 0.125rem;
 }
 </style>
