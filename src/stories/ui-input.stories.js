e251abc4c05e21d7e9158c78ff9a7a4758e61b55
alexandros <alexandros@funda.nl>
2020-12-07 17:18:53 +0100

    feat(pre/suffix-example): Update example

--- src/stories/ui-input.stories.js
+++ src/stories/ui-input.stories.js
@@ -60,7 +60,7 @@ Input.args = {
 const TemplatePrefixSufix = (args, { argTypes }) => ({
     props: Object.keys(argTypes),
     components: { UiInput },
-    template: `<ui-input @change="onChange" :type="type" :isValid="isValid" :placeHolder="placeHolder" :value="value">
+    template: `<ui-input @change="onChange" :type="type" :isValid="isValid" :placeHolder="placeHolder" :value="value" :disabled="disabled">
                 <template v-slot:prefix><div class="text-light-1 py-2 px-4 absolute top-0 left-0">PF</div></template>
                 <template v-slot:sufix><div class="text-light-1 py-2 px-4 absolute top-0 right-0">SF</div></template>
                </ui-input>`,
