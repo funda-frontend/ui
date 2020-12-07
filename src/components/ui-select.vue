d67960c150472df6a75a3224b8c02b8d68c842f7
alexandros <alexandros@funda.nl>
2020-12-07 17:35:45 +0100

    feat(input-select): style disabled and used tailwind variant

--- src/components/ui-select.vue
+++ src/components/ui-select.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="relative">
         <select
-            class="appearance-none cursor-pointer w-full rounded-sm border bg-white shadow-sm py-2 px-4 outline-none text-base font-normal h-11"
+            class="appearance-none cursor-pointer w-full rounded-sm border bg-white shadow-sm py-2 px-4 outline-none text-base font-normal h-11 disabled:bg-light-3"
+            :disabled="disabled"
             :class="{
                 'border-light-1 focus:border-dark-3': isValid,
                 'border-red-1 focus:border-red-1': !isValid,
+                'bg-light-3': disabled,
             }"
             v-bind="$attrs"
             v-on="$listeners"
@@ -18,7 +20,11 @@
             </option>
         </select>
         <div class="absolute p-2 inline-flex right-0 top-0 pointer-events-none">
-            <ui-icon icon="arrowDown" class="text-blue-2"></ui-icon>
+            <ui-icon
+                icon="arrowDown"
+                class="text-blue-2"
+                :class="{ 'text-dark-3': disabled }"
+            ></ui-icon>
         </div>
     </div>
 </template>
@@ -41,6 +47,10 @@ export default {
         isValid: {
             type: Boolean,
             default: true,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
         },
     },
 };
