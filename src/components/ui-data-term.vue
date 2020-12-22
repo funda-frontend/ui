c7ed778dd9711528e2a01f649e45c96b24440078
Ariadine Gomes <ariadine@funda.nl>
2020-12-22 10:23:24 +0100

    fix(classnames): remove unused

--- src/components/ui-data-term.vue
+++ src/components/ui-data-term.vue
@@ -1,6 +1,6 @@
 <template>
     <dt
-        class="ui-data-list__dt w-full block text-dark-3 font-normal md:py-3 pt-3 md:border-b border-light-2 md:max-w-1/3 md:flex-1/3"
+        class="w-full block text-dark-3 font-normal md:py-3 pt-3 md:border-b border-light-2 md:max-w-1/3 md:flex-1/3"
         :class="{ 'md:px-6 px-5': card }"
     >
         <slot />
