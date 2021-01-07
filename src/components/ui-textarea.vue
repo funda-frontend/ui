5abc07408b58560b913c66020cbf0bf12ac771c1
alexandros <alexandros@funda.nl>
2021-01-07 13:45:34 +0100

    fix(txtarea): removed the default value logic to make it reactive with v-model

--- src/components/ui-textarea.vue
+++ src/components/ui-textarea.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="relative">
         <textarea
-            v-model="currentValue"
             class="w-full rounded-sm border bg-white shadow-sm py-2 px-4 outline-none text-base font-normal"
             :class="{
                 'border-light-1 focus:border-dark-3': isValid,
@@ -16,24 +15,10 @@
 
 <script>
 export default {
-    inheritAttrs: false,
     props: {
         isValid: {
             type: Boolean,
             default: true,
-        },
-    },
-    data() {
-        return {
-            currentValue: this.getDefaultValue(),
-        };
-    },
-    methods: {
-        getDefaultValue() {
-            if (this.$slots.default && this.$slots.default.length) {
-                return this.$slots.default[0].text;
-            }
-            return '';
         },
     },
 };
