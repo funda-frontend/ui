bcf7bdd22306e3f97fec67371fc61b1cd59113a3
Ariadine Gomes <ariadine@funda.nl>
2021-04-08 16:58:18 +0200

    fix(modal): only set full height when using wideMode

--- src/components/ui-modal.vue
+++ src/components/ui-modal.vue
@@ -3,12 +3,15 @@
         <div :class="customModalContainerClasses" data-test-modal-container>
             <div>
                 <div
-                    class="modal-backdrop absolute inset-0 bg-opacity-60"
+                    class="bg-black absolute inset-0 bg-opacity-60"
                     @click="close"
                 ></div>
             </div>
             <div
-                :class="[customModalClasses, wideMode ? '' : 'md:max-w-2xl']"
+                :class="[
+                    customModalClasses,
+                    wideMode ? 'h-full' : 'md:max-w-2xl max-h-full',
+                ]"
                 role="dialog"
                 aria-labelledby="modalTitle"
                 aria-describedby="modalDescription"
@@ -20,13 +23,20 @@
                     </h2>
                     <button
                         v-if="showCloseIcon"
-                        class="focus:outline-none absolute w-11 h-11"
+                        class="focus:outline-none absolute w-11 h-11 top-1.5 right-1.5 p-2"
                         @click="close"
                     >
                         <ui-icon icon="close" class="text-blue-2" />
                     </button>
                 </header>
-                <section v-if="$slots.body" class="overflow-y-auto max-h-120">
+                <section
+                    v-if="$slots.body"
+                    :class="[
+                        noScroll
+                            ? 'overflow-hidden'
+                            : 'overflow-y-auto max-h-120',
+                    ]"
+                >
                     <slot name="body"></slot>
                 </section>
                 <footer
@@ -58,10 +68,14 @@ export default {
             type: Boolean,
             default: false,
         },
+        noScroll: {
+            type: Boolean,
+            default: false,
+        },
         customModalClasses: {
             type: String,
             default:
-                'w-full h-full m-auto bg-white md:rounded shadow-xl transition duration-300 ease-in-out relative overflow-hidden flex flex-col',
+                'w-full m-auto bg-white md:rounded shadow-xl transition duration-300 ease-in-out relative overflow-hidden flex flex-col',
         },
     },
     beforeMount() {
@@ -82,17 +96,3 @@ export default {
     },
 };
 </script>
-
-<style scoped>
-/* top/right available in Tailwind v2 not in v1.9.0*/
-button {
-    top: 0.375rem;
-    right: 0.375rem;
-    padding: 0.6rem;
-}
-
-/* TODO: extend tailwind config with specific background opacity */
-.modal-backdrop {
-    background: rgba(0, 0, 0, 0.6);
-}
-</style>
