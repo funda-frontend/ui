e26296bab253116bee8bcb7ac55ec09b502e04e7
Ariadine Gomes <ariadine@funda.nl>
2021-04-09 10:54:23 +0200

    fix(button): custom disabled classes per tone

--- src/components/ui-button.vue
+++ src/components/ui-button.vue
@@ -35,32 +35,42 @@ export default {
     },
     computed: {
         cssClasses() {
+            const withPadding = 'px-6 border';
+            const whiteText = 'text-white hover:text-white';
+            const blueText = 'text-blue-2 hover:text-blue-1';
+            const blueTextInverted = 'text-blue-1 hover:text-blue-2';
+            const disabledBackground =
+                'bg-light-1 border-light-1 cursor-not-allowed ';
+            const disabledBorder =
+                'text-light-1 border-light-1 cursor-not-allowed ';
+
             const specificClasses = {
-                primary:
-                    'px-6 border bg-orange-2 border-orange-2 text-white shadow hover:bg-orange-1 hover:border-orange-1 hover:text-white',
-                'primary-alt':
-                    'px-6 border bg-blue-2 text-white border-blue-2 shadow hover:bg-blue-1 hover:border-blue-1',
-                secondary:
-                    'px-6 border bg-white border-blue-2 text-blue-2 hover:text-blue-1 hover:bg-blue-5 hover:border-blue-1 shadow',
-                'secondary-alt':
-                    'px-6 border bg-blue-5 border-blue-2 text-blue-1 hover:bg-white hover:text-blue-2 hover:border-blue-2 shadow',
-                tertiary: 'text-blue-2 border-0 hover:text-blue-1',
-                transparent:
-                    'px-6 border text-white bg-transparent hover:bg-white hover:bg-opacity-25',
-                danger:
-                    'px-6 border bg-red-1 border-red-1 text-white hover:bg-red-3 hover:border-red-3 hover:text-white',
+                primary: `bg-orange-2 border-orange-2 hover:bg-orange-1 hover:border-orange-1 shadow ${withPadding} ${whiteText}`,
+                'primary-alt': `bg-blue-2 border-blue-2 hover:bg-blue-1 hover:border-blue-1 shadow ${withPadding} ${whiteText}`,
+                secondary: `bg-white border-blue-2 hover:bg-blue-5 hover:border-blue-1 shadow ${withPadding} ${blueText}`,
+                'secondary-alt': `bg-blue-5 border-blue-2 hover:bg-white hover:border-blue-2 shadow ${withPadding} ${blueTextInverted}`,
+                tertiary: `${blueText}`,
+                transparent: `bg-transparent hover:bg-white hover:bg-opacity-25 ${withPadding} ${whiteText}`,
+                danger: `bg-red-1 border-red-1 hover:bg-red-3 hover:border-red-3 shadow ${withPadding} ${whiteText}`,
+            }[this.tone];
+            const disabledClasses = {
+                primary: `shadow ${withPadding} ${disabledBackground} ${whiteText}`,
+                'primary-alt': `shadow ${withPadding} ${disabledBackground} ${whiteText}`,
+                secondary: `shadow bg-white ${withPadding} ${disabledBorder}`,
+                'secondary-alt': `shadow ${withPadding} ${disabledBorder}`,
+                tertiary: 'text-light-1',
+                transparent: `${withPadding} ${disabledBorder}`,
+                danger: `shadow ${withPadding} ${disabledBackground} ${whiteText}`,
             }[this.tone];
             return (
                 this.defaultClasses +
-                (this.disabled ? this.disabledClasses : specificClasses)
+                (this.disabled ? disabledClasses : specificClasses)
             );
         },
     },
     created() {
         this.defaultClasses =
             'leading-6 inline-flex h-11 whitespace-no-wrap border-solid rounded-sm align-middle cursor-pointer text-center items-center justify-center focus:outline-none ';
-        this.disabledClasses =
-            'px-6 border bg-light-1 border-light-1 text-light-2 shadow hover:bg-light-1 hover:border-light-1 hover:text-light-2 cursor-not-allowed ';
     },
 };
 </script>
