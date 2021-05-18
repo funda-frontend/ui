c1031ba07d9d04075a9b6d5780634994a7097437
Lars Schuitema <info@larsschuitema.nl>
2021-05-18 11:21:35 +0200

    test(button): add broken test

--- src/__tests__/ui-button.test.js
+++ src/__tests__/ui-button.test.js
@@ -6,9 +6,16 @@ const ATTR_BTN = '[data-test-button]';
 describe('UiButton', () => {
     let wrapper;
     beforeEach(() => {
-        wrapper = shallowMount(UiButton, {
-            propsData: {},
+        wrapper = shallowMount(UiButton);
+    });
+
+    it('Should render anchor element when href is set', async () => {
+        const anchor = wrapper.find('a');
+        wrapper.setProps({
+            href: '#',
         });
+        await wrapper.vm.$nextTick();
+        expect(anchor.exists()).toBe(true);
     });
 
     it('Should have tone classes when tone is set', async () => {
@@ -17,7 +24,16 @@ describe('UiButton', () => {
             tone: 'primary',
         });
         await wrapper.vm.$nextTick();
-        expect(wrapper.vm.tone).toBe('primary');
         expect(button.classes()).toContain('bg-orange-2');
+    });
+
+    it('Should disable button when disable property is set', async () => {
+        const button = wrapper.find(ATTR_BTN);
+        wrapper.setProps({
+            tone: 'secondary',
+            disabled: true,
+        });
+        await wrapper.vm.$nextTick();
+        expect(button.classes()).toContain('cursor-not-allowed');
     });
 });
