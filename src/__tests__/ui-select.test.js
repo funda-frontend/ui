e384b987e5086fe46ed5265358803a50186eda98
Lars Schuitema <info@larsschuitema.nl>
2021-05-21 14:58:01 +0200

    test(select): rewrite description

--- src/__tests__/ui-select.test.js
+++ src/__tests__/ui-select.test.js
@@ -36,7 +36,7 @@ describe('UiSelect', () => {
         options = select.findAll('option');
     });
 
-    it('Select is valid', async () => {
+    it('Should set border classes when isValid is set', async () => {
         await wrapper.setProps({ isValid: false });
         expect(wrapper.vm.isValid).toBe(false);
         expect(select.classes()).toContain('border-red-1');
@@ -46,28 +46,28 @@ describe('UiSelect', () => {
         expect(select.classes()).toContain('border-light-1');
     });
 
-    it('Select is disabled', async () => {
+    it('Should disable select when disabled prop is set', async () => {
         await wrapper.setProps({ disabled: true });
         expect(select.element.disabled).toBe(true);
         expect(select.classes()).toContain('bg-light-3');
     });
 
-    it('Option is selected', async () => {
+    it('Should select option when option is clicked', async () => {
         await options.at(2).setSelected();
         expect(wrapper.find('option:checked').element.value).toBe('floorarea');
     });
 
-    it('Option is disabled', () => {
+    it('Should disable option when disabled prop is set', () => {
         expect(options.at(2).element.disabled).toBe(true);
         expect(options.at(3).element.disabled).toBe(false);
     });
 
-    it('Option is hidden', () => {
+    it('Should hide option when hidden prop is set', () => {
         expect(options.at(2).isVisible()).toBe(true);
         expect(options.at(3).isVisible()).toBe(false);
     });
 
-    it('Option text is set', () => {
+    it('Should set text when text prop is set', () => {
         expect(options.at(1).text()).toBe('Asking price');
     });
 });
